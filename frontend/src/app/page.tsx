cea0bdf5b443b58935853c0644e7102d61b9a1d7
Zishan-125 <zafarahmed01071969@gmail.com>
2025-09-30 10:09:07 +0600

    Commented out Categories dropdown section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,12 +2,15 @@ import Nav from '../app/nav'
 import Hero from '../app/hero'
 import Footer from '../app/footer'
 import ProductPage from '../app/products/page'
+import { products } from "../data/products";
+import FeaturedProduct from './FeaturedProduct';
 
 export default function Home() {
   return (
     <>
       <Nav />
       <Hero />
+      <FeaturedProduct  products={products}/>
       <ProductPage />
       <Footer />
     </>
